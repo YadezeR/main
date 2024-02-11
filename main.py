68a93a25ea87cd96888f47a0784b59e5727d74c4
YadezeR <159686064+YadezeR@users.noreply.github.com>
2024-02-11 11:32:56 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+name = "david"
+age = 13
+height = 1.62
+weight = 60
+is_student = True
+
+if age >= 18:
+    print("Вы совершеннолетний")
+else: 
+    print("Вы несовершеннолетний")
+
+print(name)
+print(age)
+print(height)
+print(weight)
+print(is_student)
+
